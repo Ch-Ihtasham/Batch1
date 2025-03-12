36741e508d7734508b7ee3420b0d7944ee168139
Ch Ihtasham <cihtasham32@gmail.com>
2025-03-13 01:25:08 +0500

    coomit

--- Projects/to-do-context-api/src/App.jsx
+++ Projects/to-do-context-api/src/App.jsx
@@ -8,6 +8,8 @@ function App() {
 
 
   const [todos, setTodos] = useState([])
+  const [copyTodo, setCopyTodo] = useState([])
+  const [filter, setFilter] = useState('all')
 
   function addTodo(todo) {
     setTodos((preV) => [todo, ...preV])
@@ -20,18 +22,48 @@ function App() {
   }
   function toggleComplete(id) {
     setTodos((preV) => preV.map((Todo) => Todo.id === id ? { ...Todo, completed: !Todo.completed } : Todo))
+    setCopyTodo((preV) => preV.map((Todo) => Todo.id === id ? { ...Todo, completed: !Todo.completed } : Todo))
+
   }
   useEffect(() => {
-    const todos = JSON.parse(localStorage.getItem("todos"))
+    const todo = JSON.parse(localStorage.getItem("todos"))
 
-    if (todos && todos.length > 0) {
-      setTodos(todos)
+    if (todo && todo.length > 0) {
+      setTodos(todo)
+      setCopyTodo(todo)
     }
+
   }, [])
 
   useEffect(() => {
     localStorage.setItem("todos", JSON.stringify(todos))
+
   }, [todos])
+
+  useEffect(() => {
+    if (filter === 'all') {
+      setCopyTodo(todos)
+    }
+    else if (filter === 'pending') {
+      setCopyTodo(todos.filter((todo) => !todo.completed))
+    }
+    else if (filter === 'completed') {
+      setCopyTodo(todos.filter((todo) => todo.completed))
+    }
+  }, [todos, filter])
+
+
+  function all() {
+    setFilter('all')
+  }
+
+  function pendingTodos() {
+    setFilter('pending')
+  }
+  function completedTodos() {
+    setFilter('completed')
+  }
+
   return (
     <TodoProvider value={{ todos, addTodo, updateTodo, deleteTodo, toggleComplete }}>
       <div className="bg-[#172842] min-h-screen py-8">
@@ -41,13 +73,20 @@ function App() {
             {/* Todo form goes here */}
             <TodoForm />
           </div>
+          <div className='flex items-center justify-evenly my-3'>
+            <button className='rounded px-3 py-1 bg-green-600 text-white' onClick={all}>All</button>
+            <button className='rounded px-3 py-1 bg-green-600 text-white' onClick={pendingTodos}>Pending</button>
+            <button className='rounded px-3 py-1 bg-green-600 text-white' onClick={completedTodos}>Completed</button>
+          </div>
           <div className="flex flex-wrap gap-y-3">
+
             {/*Loop and Add TodoItem here */}
-            {todos.map((todo) => (
+            {copyTodo.map((todo) => (
               <div className='w-full' key={todo.id}>
                 <TodoItems todo={todo} />
               </div>
             ))}
+
 
           </div>
 
